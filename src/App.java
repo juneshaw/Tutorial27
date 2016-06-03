02924517dea08b99734abee7f4ca6db875ebce5a
June Shaw <june.shaw@me.com>
2016-06-03 16:47:49 -0600

    initial commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+
+public class App {
+
+	public static void main(String[] args) {
+
+		byte bytevalue = 127;
+		short shortvalue = 1222;
+		int intvalue = 126;
+		long longvalue = 1333333;
+		
+		float floatvalue = 133;
+		float floatvalue1 = 133.0f;
+		
+		double doublevalue = 1.2;
+		
+		bytevalue = (byte)intvalue;
+		System.out.println(bytevalue);
+		
+	}
+
+}
